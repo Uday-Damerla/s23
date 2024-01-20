76289aa6678a8611e7a8c7ab08d9811fa27ee31a
Uday Damerla <105291338+Uday-Damerla@users.noreply.github.com>
2024-01-20 19:44:33 +0530

    Update NavMenu.tsx

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import NextLink from 'next/link';
+import { useState, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import header from '../images/header.png';
 import NavItem from './NavItem';
@@ -43,13 +43,65 @@ export default function NavMenu({ className }: { className?: string }) {
       <div onClick={handleHeaderClick} style={{ margin: 0, padding: 0 }}>
         <Image src={header} alt="header" height={170} />
       </div>
-      {isNavVisible && (
-        <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 0, padding: 0 }}>
+      <Transition show={isNavVisible} as={Fragment}>
+        <nav className="fixed w-full bg-darkgrey text-lightgrey transition-transform duration-200 transform translate-y-0 lg:translate-y-full" style={{ justifyContent: 'center', alignItems: 'center', margin: 0, padding: 0 }}>
           {navItems.map((item, index) => (
             <NavItem key={index} href={item.href} text={item.text} />
           ))}
         </nav>
-      )}
+      </Transition>
+      <Transition show={isNavVisible} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto bg-gray-800 lg:hidden"
+          onClose={setNavVisible}
+        >
+          <div className="min-h-screen px-4 text-center text-gray-200">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Navigation
+                </Dialog.Title>
+                <div className="mt-2">
+                  <nav>
+                    {navItems.map((item, index) => (
+                      <NavItem key={index} href={item.href} text={item.text} />
+                    ))}
+                  </nav>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 }
